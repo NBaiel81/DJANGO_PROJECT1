1b4057133f5e94a94b2b97d5a01d5e802e8f80b3
NBaiel81 <nurmatbekov0771503907@gmail.com>
2021-02-08 18:02:41 +0600

    burger xD

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,5 +3,6 @@ from .views import *  # homepage,us,cont,register,user_list,order
 
 urlpatterns = [
     path('blog/',blog),
-    path('post/',post)
+    path('post/',post),
+    path('blog_p/<int:blog_id>',blog_posts)
     ]
